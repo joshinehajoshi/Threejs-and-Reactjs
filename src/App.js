dc05bb955d09ff23e94f347a023bc7a55adcb7e3
Neha Joshi <nehajoshigkp@gmail.com>
2022-01-19 13:12:55 +0530

    box.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import {Canvas} from '@react-three/fiber';
+import Box from './Components/Box';
 
 function App() {
   return (
     <div className="App">
-  
+      <Canvas>
+        <Box />
+      </Canvas>
     </div>
   );
 }
